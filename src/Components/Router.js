2b10f01b5a00b155e02e43b3d7a91b0af2858968
SNUPI <47105088+joohaem@users.noreply.github.com>
2021-06-29 18:45:25 +0900

    React Router with exact, Redirect, Switch

---
+++ src/Components/Router.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { BrowserRouter, Route, Redirect, Switch } from "react-router-dom";
+import Home from "Routes/Home";
+import TV from "Routes/TV";
+import Search from "Routes/Search";
+
+export default () => (
+  <BrowserRouter>
+    <Switch>
+      <Route path="/" exact component={Home} />
+      <Route path="/tv" exact component={TV} />
+      <Route path="/search" exact component={Search} />
+      <Redirect from="*" to="/" />
+    </Switch>
+  </BrowserRouter>
+);

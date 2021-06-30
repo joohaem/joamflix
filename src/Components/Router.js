ba57410e93890597432901c431fd2e7ef3c0c7cf
SNUPI <47105088+joohaem@users.noreply.github.com>
2021-07-01 00:09:27 +0900

    css with styled-component & styled-reset & withRouter

--- src/Components/Router.js
+++ src/Components/Router.js
@@ -1,16 +1,20 @@
 import React from "react";
 import { BrowserRouter, Route, Redirect, Switch } from "react-router-dom";
+import Header from "Components/Header";
 import Home from "Routes/Home";
 import TV from "Routes/TV";
 import Search from "Routes/Search";
 
 export default () => (
   <BrowserRouter>
-    <Switch>
-      <Route path="/" exact component={Home} />
-      <Route path="/tv" exact component={TV} />
-      <Route path="/search" exact component={Search} />
-      <Redirect from="*" to="/" />
-    </Switch>
+    <>
+      <Header />
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/tv" exact component={TV} />
+        <Route path="/search" exact component={Search} />
+        <Redirect from="*" to="/" />
+      </Switch>
+    </>
   </BrowserRouter>
 );
